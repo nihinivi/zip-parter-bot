54c1e703849209e8f661f72ea3bbfb2f94f617e0
Nihinivi <48147110+nihinivi@users.noreply.github.com>
2020-07-14 12:43:09 +0530

    Add files via upload

---
+++ fileparterbot.py
@@ -0,0 +1,44 @@
+from telethon.sync import TelegramClient 
+from telethon import events
+import os
+from zipfile import ZipFile,ZIP_DEFLATED
+from uuid import uuid4
+from os import environ
+globals().update(environ)
+bot = TelegramClient('bot', api_id, api_hash).start(bot_token=TOKEN)
+
+def absoluteFilePaths(directory):
+   for dirpath,_,filenames in os.walk(directory):
+       for f in filenames:
+           yield os.path.abspath(os.path.join(dirpath, f))
+
+async def zipper(filelist,name):
+    with ZipFile(name, 'w') as zipMe:        
+        for file in filelist:
+            zipMe.write(file, compress_type=ZIP_DEFLATED)
+    
+async def hey(event):
+    reply_message = await event.get_reply_message()
+    await event.reply("Starting to part the files please wait...")
+    name = await bot.download_media(reply_message,"./")
+    dir = str(uuid4())
+    os.mkdir(dir)
+    ZipFile(name).extractall(dir)
+    files = list(absoluteFilePaths(dir))
+    n = 30
+    k = [files[i:i + n] for i in range(0, len(files), n)]  
+    ids = []
+    for i in k:
+        zipname = str(uuid4())+".zip"
+        print(zipname)
+        ids.append(zipname)
+        await zipper(i,zipname)
+    for id in ids:     
+        await event.reply(file=id)
+    os.system("rm -rf *")
+
+
+        
+print("---Started bot---")     
+bot.add_event_handler(hey,events.NewMessage(pattern="/part"))
+bot.run_until_disconnected()
